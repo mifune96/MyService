39fd4c097a4fa65ac7002b9b92491e338aca3a97
mifune96 <imrank.sensea@gmail.com>
2021-12-23 10:09:30 +0700

    finish

--- app/src/main/java/com/tomuchcoffee/myservice/MainActivity.kt
+++ app/src/main/java/com/tomuchcoffee/myservice/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.tomuchcoffee.myservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStartService = findViewById<Button>(R.id.btn_start_service)
+        btnStartService.setOnClickListener {
+            val mStartServiceIntent = Intent(this, MyService::class.java)
+            startService(mStartServiceIntent)
+        }
+        val btnStartJobIntentService = findViewById<Button>(R.id.btn_start_job_intent_service)
+        btnStartJobIntentService.setOnClickListener {
+
+        }
+        val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {
+
+        }
+        val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener {
+
+        }
     }
 }
