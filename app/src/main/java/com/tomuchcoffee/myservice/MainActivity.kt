7494c6a8650782f51f326aed1a24b5fb2e37cd71
mifune96 <imrank.sensea@gmail.com>
2021-12-24 09:38:27 +0700

    finish

--- app/src/main/java/com/tomuchcoffee/myservice/MainActivity.kt
+++ app/src/main/java/com/tomuchcoffee/myservice/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.tomuchcoffee.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var mServiceBount = false
+    private lateinit var myBoundService: MyBoundService
+
+    private val mServiceConnection = object : ServiceConnection {
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mServiceBount = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            myBoundService = myBinder.getService
+            mServiceBount = true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,11 +44,20 @@ class MainActivity : AppCompatActivity() {
         }
         val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
         btnStartBoundService.setOnClickListener {
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
 
         }
         val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
         btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if(mServiceBount){
+            unbindService(mServiceConnection)
         }
     }
 }
